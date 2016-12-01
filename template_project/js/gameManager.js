09964a0cd3436a65a7646993c3e4d8a5f4e65155
simonlary <simonlary@gmail.com>
2016-11-30 19:18:37 -0500

    Finish adding comments

--- template_project/js/gameManager.js
+++ template_project/js/gameManager.js
@@ -1,11 +1,15 @@
-var gameManager;
-var canvas;
-
+//////////////////////////////////////////////////////////
+//	This is the script that instantiates the engine and	//
+// 	loads the level contained in 'level.js'.			//
+//////////////////////////////////////////////////////////
 const init = function () {
-	canvas = document.getElementsByTagName("canvas")[0];
+	// Find the canvas we want the engine to use. In this case
+	// it is the only canvas in the whole HTML page.
+	let canvas = document.getElementsByTagName("canvas")[0];
 
-	// Create engine
+	// Create the engine with the provided canvas
 	new Engine(canvas);
-	engine.LoadLevel(l1);
-	engine.Camera.size = 10;
+
+	// Load the pre-made level with engine.LoadLevel()
+	engine.LoadLevel(level);
 };
