31dda017e68296d6f6c1e22f8efc78eeec9d20cd
simonlary <simonlary@users.noreply.github.com>
2016-12-01 19:51:42 -0500

    Comment template project (#4)
    * Comments the HTML and CSS files.
    
    * Finish adding comments

--- template_project/js/level.js
+++ template_project/js/level.js
@@ -1,30 +1,62 @@
-const l1 = {
-	"name": "Level 1",
+//////////////////////////////////////////////////////////////////
+//	This is the level that we are loading in 'gameManager.js'.	//
+// 	We declare it in a separate file because of its size.		//
+//////////////////////////////////////////////////////////////////
+const level = {
+	// The name of this level
+	"name": "My Awesome Level",
+
+	// The gravity vector in this level. In this case, there is no gravity
 	"gravity": { "x": 0, "y": 0 },
+
+	// The array of all the GameObjects that have to be instantiated,
+	// and all the components they contain
 	"gameObjects": [
+
+		// GameObject 1 //
+		//  A red box   //
+		//////////////////
 		{
+			// Add a 'Transform' component
 			"Transform": {
+				// Sets the initial position to (0,0)
 				"position": { "x": 0, "y": 0 },
+				// Give this GameObject the name "Box1"
 				"name": "Box1"
 			},
+			// Add a 'SpriteRenderer' component
 			"SpriteRenderer": {
+				// Give it a width of 1
 				"width": 1,
+				// and a height of 2
 				"height": 2,
+				// Use the image located at "img/square_red.png" for display
 				"sprite": "img/square_red.png"
 			},
+			// Add a 'BoxCollider' component
 			"BoxCollider": {
+				// Give it a width of 1
 				"width": 1,
+				// and a height of 2
 				"height": 2,
+				// Give it a coefficient of restitution of 1 (all the energy is conserved)
 				"e": 1
 			},
+			// Add a 'MovingElement' component
 			"MovingElement": {
+				// Give it a mass of 10
 				"mass": 10,
+				// Sets it initial velocity to (10,3)
 				"velocity": {
 					"x": 10,
 					"y": 3
 				}
 			}
 		},
+
+		// GameObject 2 //
+		//  A blue box  //
+		//////////////////
 		{
 			"Transform": {
 				"position": { "x": 5, "y": 2 },
@@ -48,6 +80,10 @@ const l1 = {
 				}
 			}
 		},
+
+		// GameObject 3 //
+		//  West Wall   //
+		//////////////////
 		{
 			"Transform": {
 				"position": { "x": -8, "y": 0 },
@@ -68,6 +104,10 @@ const l1 = {
 				"fixed": true
 			}
 		},
+
+		// GameObject 4 //
+		//  East Wall   //
+		//////////////////
 		{
 			"Transform": {
 				"position": { "x": 8, "y": 0 },
@@ -88,6 +128,10 @@ const l1 = {
 				"fixed": true
 			}
 		},
+
+		// GameObject 5 //
+		//  South Wall  //
+		//////////////////
 		{
 			"Transform": {
 				"position": { "x": 0, "y": -4 },
@@ -108,6 +152,10 @@ const l1 = {
 				"fixed": true
 			}
 		},
+
+		// GameObject 6 //
+		//  North Wall  //
+		//////////////////
 		{
 			"Transform": {
 				"position": { "x": 0, "y": 4 },
